cff11987458ad425ea63f8af9530546d58b31d0b
eki <elmedinmustafic123@gmail.com>
2021-09-09 20:37:15 +0200

    fetch

--- training/JS tasks/HighestScoringWord.js
+++ training/JS tasks/HighestScoringWord.js
@@ -1,94 +1,20 @@
 function high(x){
 
     arg1 = x.split(" ")
+    slova = " abcdefghijklmnopqrstuvwxyz"
     brojac = []
     
     for(i=0; i<arg1.length; i++){
         brojac.push(0)
         for(j=0; j<arg1[i].length; j++){
-            switch(arg1[i][j]){
-                case 'a':
-                    brojac[i] += 1;
-                    break
-                case 'b':
-                    brojac[i] += 2;
-                    break
-                case 'c':
-                    brojac[i] += 3;
-                    break
-                case 'd':
-                    brojac[i] += 4;
-                    break
-                case 'e':
-                    brojac[i] += 5;
-                    break
-                case 'f':
-                    brojac[i] += 6;
-                    break
-                case 'g':
-                    brojac[i] += 7;
-                    break
-                case 'h':
-                    brojac[i] += 8;
-                    break
-                case 'i':
-                    brojac[i] += 9;
-                    break
-                case 'j':
-                    brojac[i] += 10;
-                    break
-                case 'k':
-                    brojac[i] += 11;
-                    break
-                case 'l':
-                    brojac[i] += 12;
-                    break
-                case 'm':
-                    brojac[i] += 13;
-                    break
-                case 'n':
-                    brojac[i] += 14;
-                    break
-                case 'o':
-                    brojac[i] += 15;
-                    break
-                case 'p':
-                    brojac[i] += 16;
-                    break
-                case 'q':
-                    brojac[i] += 17;
-                    break
-                case 'r':
-                    brojac[i] += 18;
-                    break
-                case 's':
-                    brojac[i] += 19;
-                    break
-                case 't':
-                    brojac[i] += 20;
-                    break
-                case 'u':
-                    brojac[i] += 21;
-                    break
-                case 'v':
-                    brojac[i] += 22;
-                    break
-                case 'w':
-                    brojac[i] += 23;
-                    break
-                case 'x':
-                    brojac[i] += 24;
-                    break
-                case 'y':
-                    brojac[i] += 25;
-                    break
-                case 'z':
-                    brojac[i] += 26;
-                    break
+            for(k=1; k<slova.length; k++){
+                if(arg1[i][j]===slova[k]){
+                    brojac[i] += k
+                }
             }
         }
     }
-        idnx = brojac.indexOf(Math.max(...brojac))
+        indx = brojac.indexOf(Math.max(...brojac))
         return arg1[indx]
     }
     console.log(high('mana i need a taxi up to ubud'))
--- training/vezbanje1.js
+++ training/vezbanje1.js
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 // while(true){
 //     operacija = prompt("unesite operaciju");
 //     if(operacija === "+"){
@@ -86,4 +87,107 @@
 // console.log(pointInCircle(0.7,-0.7))
 
 
- 
+//  let str = 'ja radim sam'
+//  arg = str.split(' ')
+//  str = arg.join('')
+//  console.log(str)
+
+
+// function element(arg){
+//     res = []
+//     res = arg.filter((el) => Boolean(el));
+//     return res
+// }console.log(element([NaN,0,15,"",2]))
+
+// const person = {
+//     firstName : "John",
+//     lastName  : "Doe",
+//     age     : 50,
+//     eyeColor  : "blue",
+//     // fullname: function(){
+//     //    return person.firstName + " " + person.lastName
+//     // }
+//   }
+//   console.log(Object.entries(person))
+//   console.log(JSON.stringify(person))
+//   txt = " "
+//   for(let x in person){
+//       txt += person[x] + " "
+//   }
+//   console.log(txt)
+
+
+// var library = [
+//     {
+//         author: "Bill Gates",
+//         title: 'The Road Ahead',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Steve Jobs',
+//         title: 'Walter Isaacson',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Suzanne Collins',
+//         title:  'Mockingjay: The Final Book of The Hunger Games', 
+//         readingStatus: false
+//     }
+// ]
+// library.forEach(el => {
+//     console.log(el.readingStatus)
+// });
+
+// function url(arg){
+
+//     rpl1 = arg.replace("/","")
+//     rpl2 = rpl1.replace(":","")
+//     spl = rpl2.split("/")
+//     spl1 = spl[1].split("").slice(4,).join("")
+//     console.log(spl)
+
+//     obj = {
+//         Protocol: spl[0],
+//         Domain: spl1,
+//         Ost1: spl[2],
+//         Ost2: spl[3]
+//     }
+//     return obj
+// }
+// console.log(url("https://www.w3resource.com/javascript-exercises/javascript-object-exercises.php"))
+
+
+// obj = {
+//     first: "f1",
+//     second: "s2",
+//     third: "t3"
+// }
+// obj1 = {}
+// reversed = []
+// Object.entries(obj).forEach(el => {
+//     reversed.push(el.reverse());
+// });
+
+// for (let i = 0; i < reversed.length; i++) {
+//     for(let j = 0; j < reversed[i].length; j++){
+//         Reflect.set(obj1,reversed[i][j],reversed[i][j+1])
+//             break;
+//     }
+// }
+
+// console.log(obj1)
+
+// function incl(x){
+//     obj = {
+//         name: "eki",
+//         age: 26,
+//         work: "school"
+//     }
+//     if(Object.keys(obj).includes(x)){
+//         return true
+//     }else{
+//         return false
+//     }
+// }console.log(incl("name"))
+
+fetchingData = fetch()
